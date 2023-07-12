c7e81524143e2bfed2fd983ba4e42f1b6771bf61
carxeco <56443968+carxeco@users.noreply.github.com>
2023-07-12 15:40:37 +0100

    Bad Omens Setlist
    Bad Omens Setlist Website

---
+++ app.js
@@ -0,0 +1,59 @@
+let viz
+
+// Create varianble to store viz container
+const vizContainer = document.getElementById("vizContainer");
+
+
+// Create Variable to store URL
+const options = {
+    device: "desktop",
+    height: "1200px",
+    width: "1600px",
+    hideToolbar: true,
+   
+};
+
+
+// Create variable to store the URL
+const url = "https://public.tableau.com/views/BadOmensMusicSetlistDashboard/OverallView";
+
+
+//Define Function to our Dashboard
+function initviz(){
+    viz = new tableau.Viz(vizContainer, url, options);
+}
+
+//Execute function to load dashabord after website loads
+document.addEventListener("DOMContentLoaded", initviz);
+
+//Create variable to store the export pdf button
+const exportPDFButton = document.getElementById("exportPDF");
+
+//Define a function to trigger on button click
+function exportPDFfunction(){
+    viz.showExportPDFDialog();
+};
+
+//Add event listener for when the button is clicked
+exportPDFButton.addEventListener("click", exportPDFfunction);
+
+//Create variable to store the export powerpoint button
+const exportPowerPointButton = document.getElementById("exportPP");
+
+//Define a function to trigger on button click
+function exportPowerPointfunction(){
+    viz.showExportPowerPointDialog();
+};
+
+//Add event listener for when the button is clicked
+exportPowerPointButton.addEventListener("click", exportPowerPointfunction);
+
+// Set Filter interaction
+function getDatevalues(){
+    const dateValue=document.getElementById
+    ("dateVal").value;
+    console.log(dateValue);
+}
+
+const filterButton = document.getElementById("filterButton");
+filterButton.addEventListener("click", getDatevalues);
